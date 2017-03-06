982bfe6ed19291dcbe3bb8a05b12834b0399db50
jusongchen <jusong.chen@gmail.com>
2017-03-06 10:46:46 -0800

    httpsWeb with port and TLS flag

--- httpsWeb/main.go
+++ httpsWeb/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, req *http.Request) {
@@ -11,8 +14,36 @@ func handler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	port := 443
+	TLS := true
+	flag.IntVar(&port, "port", 443, "port to bind to")
+	flag.BoolVar(&TLS, "TLS", true, "https with cert.pem and key.pem for SSL certificate")
+
+	flag.Usage = func() {
+		fmt.Println("Usage:")
+		fmt.Printf("   %s [flags]  \n", os.Args[0])
+		fmt.Println("Flags:")
+		flag.PrintDefaults()
+		fmt.Printf("\nThis programm opens cert.pem and key.pem for SSL certificate")
+		os.Exit(-1)
+	}
+
+	flag.Parse()
+
+	if flag.NArg() != 0 {
+		flag.Usage()
+	}
+	portStr := fmt.Sprintf(":%d", port)
+
 	http.HandleFunc("/", handler)
-	log.Printf("About to listen on 8001.")
-	err := http.ListenAndServeTLS(":8001", "cert.pem", "key.pem", nil)
-	log.Fatal(err)
+
+	log.Print("About to listen on ", portStr)
+	if TLS {
+		err := http.ListenAndServeTLS(portStr, "cert.pem", "key.pem", nil)
+		log.Fatal(err)
+	} else {
+		err := http.ListenAndServe(portStr, nil)
+		log.Fatal(err)
+	}
+
 }
